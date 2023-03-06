1f1c1947b7021e7401783a2ca4bcf37cf9d26e7a
danakik <danakik28@gmail.com>
2023-03-06 19:05:25 +0200

    print name lesson and number task print my name,surname and number record book print name teachers print solution task

---
+++ task_1.py
@@ -0,0 +1,10 @@
+print("Multiparadigm programming languages : Task 1")
+print("Danil Mashkin, FI-9119")
+print("Vladyslav Valeriyovych, "*44,end = "Vladyslav Valeriyovych. \n" )
+num_1 = 123 - 23.1
+num_2 = 2**3
+num_2 += 2.2
+num_3 = 127.24 - 21.1
+num_4 = 3.2
+num_5 = ((num_1 * num_2) / num_3) - num_4
+print(round(num_5, 5))
