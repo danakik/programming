b10da8688b7abcb20437939f7468365c0e1d41c2
danakik <danakik28@gmail.com>
2023-03-14 23:23:08 +0200

    print name lesson, number task, my name, surname request numbers print solution task

---
+++ task_2.py
@@ -0,0 +1,9 @@
+print('Multiparadigm programming languages : Task 2')
+print('Danil Mashkin, FI-9119')
+
+x = int(input('Input x: '))
+y = int(input('Input y: '))
+z = int(input('Input z: '))
+
+num = (((z - 23.1) * ((2**x) + 2.2)) / (y - 21.1)) - 3.2
+print(round(num, 5))
