152be1f568a941654ff929df9b9e9068df0b3f28
danakik <danakik28@gmail.com>
2023-03-16 08:44:32 +0200

    print name lesson, number task, my name, surname request numbers print solution task or system error

--- task_2.py
+++ task_2.py
@@ -1,18 +1,11 @@
+import utils
+
 print('Multiparadigm programming languages : Task 2')
 print('Danil Mashkin, FI-9119')
 
-
-def validate_input(var_name):
-    while True:
-        try:
-            return int(input('Add the value {}: '.format(var_name)))
-        except ValueError:
-            print('Please enter a number')
-
-
-x = validate_input('x')
-y = validate_input('y')
-z = validate_input('z')
+x = utils.validate_input('x')
+y = utils.validate_input('y')
+z = utils.validate_input('z')
 
 result = (((z - 23.1) * ((2 ** x) + 2.2)) / (y - 21.1)) - 3.2
 print(round(result, 5))
