9d81bcfef63ae6175a6e818e7de8602251ba9d55
danakik <danakik28@gmail.com>
2023-06-01 09:03:26 +0300

    fix

--- utils.py
+++ utils.py
@@ -1,15 +1,6 @@
-def validate_input(var_name):
+def validate_input(var_name, cast_fnc=int):
     while True:
         try:
-            return int(input('{}: '.format(var_name)))
+            return cast_fnc(input(f'{var_name}: '))
         except ValueError:
             print('Please enter a number(int type)')
-
-
-def validate_input_v2(var_name):
-    while True:
-        try:
-            return int(input('{}: '.format(var_name)))
-        except ValueError:
-            print('Please enter a number')
-
