346b543a6c407b803bad78848581a28c540668fd
danakik <danakik28@gmail.com>
2023-06-02 09:00:20 +0300

    fix

---
+++ task_1/task_1.py
@@ -0,0 +1,8 @@
+print('Multiparadigm programming languages : Task 1')
+print("Danil Mashkin, FI-9119")
+
+TEACHER_45 = "Vladyslav Valeriyovych, " * 45
+print(TEACHER_45[:-2])
+
+NUM = ((123 - 23.1 * (2**3) + 2.2) / 127.24 - 21.1) - 3.2
+print(round(NUM, 5))
